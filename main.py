32b7d054ba4cd7da9f32643e9de8a9c9d4f89b28
Achleole <migliorini.arthur@gmail.com>
2020-04-22 03:16:12 -0400

    Makes the loss into a single nn.Module subclass

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import requests
 from io import BytesIO
 from matplotlib import pyplot as plt
 
+import random
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -67,83 +68,94 @@ class StyleLoss(nn.Module):
         self.target = target.detach()
 
     def forward(self, x):
-        # channel = x.shape[3]  # 1 isn't it ??
         channel = x.shape[1]
         self.loss = ( 1 / channel ** 2) * self.compute_loss(GramMatrix()(x), GramMatrix()(self.target))
         return x
 
-    
+class LossNet (nn.Module):
+    def __init__(self, children_list, content_layers, style_layers, style_img, content_img, 
+                 style_weight=1000000, content_weight=10,                          
+                 normalization_mean = cnn_normalization_mean, 
+                 normalization_std = cnn_normalization_std,
+                StyleLoss = StyleLoss,
+                ContentLoss = ContentLoss):
+        super().__init__()
+        self.style_weight=style_weight
+        self.content_weight=content_weight
+        i = 0
+        imax = max([int(name.split('_')[-1]) for name in (content_layers+style_layers)])
+        self.vgg = nn.Sequential(Normalization(normalization_mean, normalization_std).to(device))
+        self.content_losses = []
+        self.style_losses = []
+        for layer in cnn.children():
+            if isinstance(layer, nn.Conv2d):
+                i += 1
+                name = 'conv_{}'.format(i)
+            elif isinstance(layer, nn.ReLU):
+                name = 'relu_{}'.format(i)
+                # The in-place version doesn't play very nicely with the ContentLoss
+                # and StyleLoss we insert below. So we replace with out-of-place
+                # ones here.
+                layer = nn.ReLU(inplace=False)
+            elif isinstance(layer, nn.MaxPool2d):
+                name = 'pool_{}'.format(i)
+            elif isinstance(layer, nn.BatchNorm2d):
+                name = 'bn_{}'.format(i)
+                
+            if i > imax:
+                break
+                
+            self.vgg.add_module(name, layer)
+            
+            
+            if name in content_layers:
+                # add content loss:
+                target = self.vgg(content_img).detach()
+                content_loss = ContentLoss(target)
+                self.vgg.add_module("content_loss_{}".format(i), content_loss)
+                self.content_losses.append(content_loss)
 
-def get_style_model_and_losses(cnn, style_img, content_img,                               
-                               normalization_mean = cnn_normalization_mean, 
-                               normalization_std =cnn_normalization_std,
-                               content_layers=content_layers_default,
-                               style_layers=style_layers_default):
-    cnn = copy.deepcopy(cnn)
-
-    # normalization module
-    normalization = Normalization(normalization_mean, normalization_std).to(device)
-
-    # just in order to have an iterable access to or list of content/syle
-    # losses
-    content_losses = []
-    style_losses = []
-
-    # assuming that cnn is a nn.Sequential, so we make a new nn.Sequential
-    # to put in modules that are supposed to be activated sequentially
-    model = nn.Sequential(normalization)
-
-    i = 0  # increment every time we see a conv
-    for layer in cnn.children():
-        if isinstance(layer, nn.Conv2d):
-            i += 1
-            name = 'conv_{}'.format(i)
-        elif isinstance(layer, nn.ReLU):
-            name = 'relu_{}'.format(i)
-            # The in-place version doesn't play very nicely with the ContentLoss
-            # and StyleLoss we insert below. So we replace with out-of-place
-            # ones here.
-            layer = nn.ReLU(inplace=False)
-        elif isinstance(layer, nn.MaxPool2d):
-            name = 'pool_{}'.format(i)
-        elif isinstance(layer, nn.BatchNorm2d):
-            name = 'bn_{}'.format(i)
-
-        model.add_module(name, layer)
-
-        if name in content_layers:
-            # add content loss:
-            target = model(content_img).detach()
-            content_loss = ContentLoss(target)
-            model.add_module("content_loss_{}".format(i), content_loss)
-            content_losses.append(content_loss)
-
-        if name in style_layers:
-            # add style loss:
-            target_feature = model(style_img).detach()
-            style_loss = StyleLoss(target_feature)
-            model.add_module("style_loss_{}".format(i), style_loss)
-            style_losses.append(style_loss)
-
-    # now we trim off the layers after the last content and style losses
-    for i in range(len(model) - 1, -1, -1):
-        if isinstance(model[i], ContentLoss) or isinstance(model[i], StyleLoss):
-            break
-
-    model = model[:(i + 1)]
-
-    return model, style_losses, content_losses
+            if name in style_layers:
+                # add style loss:
+                target_feature = self.vgg(style_img).detach()
+                style_loss = StyleLoss(target_feature)
+                self.vgg.add_module("style_loss_{}".format(i), style_loss)
+                self.style_losses.append(style_loss)
+                
+            
+    def forward(self, x):
+        self.vgg(x)
+            
+        content_loss = 0
+        style_loss = 0
+        for content in self.content_losses:
+            content_loss += content.loss
+            
+        for style in self.style_losses:
+            style_loss += style.loss
+            
+            
+        self.style_score = style_loss * self.style_weight
+        self.content_score = content_loss * self.content_weight
+        loss = self.style_score + self.content_score
+        return loss
+        
 
 def run_style_transfer(cnn, style_img, content_img, input_img,                               
                        normalization_mean = cnn_normalization_mean, 
                        normalization_std =cnn_normalization_std,
                        num_steps=300,
                        style_weight=1000000, 
-                       content_weight=10, step = 50, save = False, show = True):
+                       content_weight=10, step = 50, save = False, show = True,
+                       content_layers=content_layers_default,
+                       style_layers=style_layers_default):
     """Run the style transfer."""
     print('Building the style transfer model..')
-    model, style_losses, content_losses = get_style_model_and_losses(cnn, style_img, content_img,
-                                                                     normalization_mean, normalization_std)
+    model = LossNet(cnn.children(), content_layers, style_layers, style_img, content_img, 
+                 style_weight = style_weight, content_weight = content_weight,                          
+                 normalization_mean = normalization_mean, 
+                 normalization_std = normalization_std)
+    
     optimizer =  optim.LBFGS([input_img.requires_grad_()])
     if save :
         tensorToPil(content_img).save("output/content_input.jpg")
@@ -161,32 +173,21 @@ def run_style_transfer(cnn, style_img, content_img, input_img,
             input_img.data.clamp_(0, 1)
 
             optimizer.zero_grad()
-            model(input_img)
-            style_score = 0
-            content_score = 0
-
-            for sl in style_losses:
-                style_score += sl.loss
-            for cl in content_losses:
-                content_score += cl.loss
-
-            style_score *= style_weight
-            content_score *= content_weight
-
-            loss = style_score + content_score
+            loss = model(input_img)
+            
             loss.backward()
             iteration += 1
             if iteration % step == 0:
                 print("iteration {}:".format(iteration))
                 print('Style Loss : {:4f} Content Loss: {:4f}'.format(
-                    style_score.item(), content_score.item()))
+                    model.style_score.item(), model.content_score.item()))
                 if show:
                     imshow(input_img)
                 if save:
-                    tensorToPil(input_img).save(f"output/output_{iteration // step}.jpg")
+                    tensorToPil(input_img).save("output/output_"+str(iteration // step)+".jpg")
                 print()
                 
-            return style_score + content_score
+            return loss
         
         optimizer.step(closure)
 
@@ -216,4 +217,4 @@ style_image = image_loader(style_path)
 content_image = image_loader(content_path)
 input_image = content_image.clone()
 cnn = models.vgg19(pretrained=True).features.to(device).eval()
-output = run_style_transfer(cnn, style_image, content_image, input_image, num_steps= 500, step = 50, save = True)
+output = run_style_transfer(cnn, style_image, content_image, input_image, num_steps= 150, step = 50, save = True)
