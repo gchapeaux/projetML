e11f551113958c39b8ab849897fdbcff53a4cf74
Grégoire Chapeaux <gchapeaux@gmail.com>
2020-04-24 19:30:48 -0400

    Adding Residual network

--- main.py
+++ main.py
@@ -139,6 +139,60 @@ class LossNet (nn.Module):
         self.content_score = content_loss * self.content_weight
         loss = self.style_score + self.content_score
         return loss
+
+class Residual (nn.Module):
+    def __init__(self):
+        super(Residual, self).__init__()
+        self.couche1 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
+        self.batchNorm1 = nn.BatchNorm2d(128)
+        self.couche2 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
+        self.batchNorm2 = nn.BatchNorm2d(128)
+        self.ReLU = F.relu
+
+    def forward(self, x):
+        y = self.ReLU(self.batchNorm1(self.couche1(x)))
+        y = self.batchNorm2(self.couche2(y))
+        y = y + x
+        return y
+
+class ImgTNet (nn.Module):
+    def __init__(self):
+        super(ImgTNet, self).__init__()
+        self.reflectionPadding = torch.nn.ReflectionPad2d(40)
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=9, stride=1)
+        self.batchNorm1 = nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=2)
+        self.batchNorm2 = nn.BatchNorm2d(64)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=2)
+        self.batchNorm3 = nn.BatchNorm2d(128)
+        self.residual1 = Residual()
+        self.residual2 = Residual()
+        self.residual3 = Residual()
+        self.residual4 = Residual()
+        self.residual5 = Residual()
+        self.conv4 = nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2)
+        self.batchNorm4 = nn.BatchNorm2d(64)
+        self.conv5 = nn.ConvTranspose2d(64, 32, kernel_size=3, stride=2)
+        self.batchNorm5 = nn.BatchNorm2d(32)
+        self.conv6 = nn.ConvTranspose2d(32, 3, kernel_size=9, stride=1)
+        self.batchNorm6 = nn.BatchNorm2d(3)
+        self.ReLU = F.relu
+        self.STanh = lambda x : 255/2*(1+nn.Tanh()(x))
+
+    def forward(self, x):
+        out = self.reflectionPadding(x)
+        out = self.ReLU(self.batchNorm1(self.conv1(out)))
+        out = self.ReLU(self.batchNorm2(self.conv2(out)))
+        out = self.ReLU(self.batchNorm3(self.conv3(out)))
+        out = self.residual1.forward(out)
+        out = self.residual2.forward(out)
+        out = self.residual3.forward(out)
+        out = self.residual4.forward(out)
+        out = self.residual5.forward(out)
+        out = self.ReLU(self.batchNorm4(self.conv4(out)))
+        out = self.ReLU(self.batchNorm5(self.conv5(out)))
+        out = self.STanh(self.batchNorm6(self.conv6(out)))
+        return out
         
 
 def run_style_transfer(cnn, style_img, content_img, input_img,                               
@@ -146,7 +200,7 @@ def run_style_transfer(cnn, style_img, content_img, input_img,
                        normalization_std =cnn_normalization_std,
                        num_steps=300,
                        style_weight=1000000, 
-                       content_weight=10, step = 50, save = False, show = True,
+                       content_weight=10, step = 50, save = False, show = False,
                        content_layers=content_layers_default,
                        style_layers=style_layers_default):
     """Run the style transfer."""
@@ -207,7 +261,9 @@ def image_loader(image_name):
 content_path   = "images/danse.jpg"
 style_path = "images/wave.jpeg"
 
-imsize = 400 if torch.cuda.is_available() else 128  # use small size if no gpu
+#TODO : Uncomment
+imsize = 256 #Network test
+#imsize = 400 if torch.cuda.is_available() else 128  # use small size if no gpu
 
 loader = transforms.Compose([
     transforms.Resize((imsize, imsize)),  # scale imported image
@@ -216,5 +272,13 @@ loader = transforms.Compose([
 style_image = image_loader(style_path)
 content_image = image_loader(content_path)
 input_image = content_image.clone()
-cnn = models.vgg19(pretrained=True).features.to(device).eval()
-output = run_style_transfer(cnn, style_image, content_image, input_image, num_steps= 150, step = 50, save = True)
+#TODO : Uncomment
+#cnn = models.vgg19(pretrained=True).features.to(device).eval()
+#output = run_style_transfer(cnn, style_image, content_image, input_image, num_steps= 150, step = 50, save = True)
+
+reseau = ImgTNet().cuda()
+image = reseau(input_image)
+print(image.shape)
+imshow(image)
+import time
+time.sleep(30)
